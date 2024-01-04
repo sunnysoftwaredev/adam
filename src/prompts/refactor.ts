948e112e8766b3183c71033e1206f3fb2f3257ee
Trevin Hofmann <trevinhofmann@gmail.com>
2024-01-04 12:30:49 -0800

    feat: further improve success rate of refactor responses with a clearer prompt

--- src/prompts/refactor.ts
+++ src/prompts/refactor.ts
@@ -2,15 +2,9 @@ import { ask } from '../gpt';
 
 const PROMPT = (code: string): string => `Hello! Please assume the role of an experienced and talented software engineer named ADAM.
 
-Below is a TypeScript file.
+I will be providing a TypeScript file at the very end of this prompt. Please consider if there are any ways that you would refactor it to improve readability or performance (without sacrificing the other).
 
-Please consider if there are any ways that you would refactor it to improve readability or performance (without sacrificing the other).
-
-I am providing the code within triple-tick-quotes. Please do the same with your code.
-
-\`\`\`
-${code}
-\`\`\`
+The code I provide will have unescaped triple-tick quotes (\`\`\`) before and after it.
 
 If you do not have any worthwhile suggestions, that is okay. Just reply by saying "No recommendations." (nothing else).
 
@@ -18,7 +12,7 @@ You should be concise but include all important details when you write commit me
 
 When you reply with your {COMPLETE_UPDATED_FILE_CONTENTS}, do not abbreviate it with comments such as "Keep this part the same." Include the ENTIRE contents of the updated file.
 
-If you do have a suggestion, please respond in EXACTLY the following format, and nothing additional (replacing curly brace parts with your corresponding response):
+If you do have a suggestion, please respond in EXACTLY the following format (including the exact newlines/whitespace and the 6 quotes before/after the code!), and nothing additional (replacing curly brace parts with your corresponding response).
 
 Title: {PULL_REQUEST_TITLE}
 
@@ -30,8 +24,14 @@ Branch name: {BRANCH_NAME}
 
 File contents:
 
-\`\`\`
+""""""
 {COMPLETE_UPDATED_FILE_CONTENTS}
+""""""
+
+Now that you understand how to respond, here is the code I would like you to review, between the triple tick quotes:
+
+\`\`\`
+${code}
 \`\`\``;
 
 type PullRequestInfo = {
@@ -46,7 +46,7 @@ const titlePattern = /Title: ([\s\S]*?)\n\n/;
 const descriptionPattern = /Description: ([\s\S]*?)\n\n/;
 const commitMessagePattern = /Commit message: ([\s\S]*?)\n\n/;
 const branchNamePattern = /Branch name: ([\s\S]*?)\n\n/;
-const contentPattern = /File contents:\n\n```\n([\s\S]*?)```/;
+const contentPattern = /File contents:\n\n""""""\n([\s\S]*?)""""""/;
 
 const getTitle = (str: string) => str.match(titlePattern)?.[1].trim() || '';
 const getDescription = (str: string) => str.match(descriptionPattern)?.[1].trim() || '';
