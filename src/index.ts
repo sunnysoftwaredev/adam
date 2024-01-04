c2a75b41dc3eebc711e18c2c28fa5d4eecd63fa3
Trevin Hofmann <trevinhofmann@gmail.com>
2024-01-04 13:01:10 -0800

    Handle all unhandled promises for better error tracing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import './dotenv';
 import demo from './demo';
 
+process.on('unhandledRejection', (reason, promise) => {
+  console.log('Unhandled Rejection at:', promise, 'reason:', reason);
+  // Application specific logging, throwing an error, or other logic here
+});
+
 demo().catch(e => {
   console.error('There was an error in the demo.', e);
 });
