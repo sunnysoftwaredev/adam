552f9f2de19d31bd830633c9108ba0e7861333f2
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-05 19:14:42 +0000

    feat: enhance 'demo' function error logging with detailed error information

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import './dotenv';
 import demo from './demo';
 
 demo().catch(e => {
-  console.error('There was an error in the demo.', e);
+  console.error('There was an error in the demo. Error Name: ', e.name, '. Error Message: ', e.message, '. Stack: ', e.stack);
 });
