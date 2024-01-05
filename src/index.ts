d85c82e0b22ba81455b376de0a0512708947db9e
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-05 19:03:58 +0000

    refactor(main): include specific error message in catch block during demo invocation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+
+```typescript
 import './dotenv';
 import demo from './demo';
 
 demo().catch(e => {
-  console.error('There was an error in the demo.', e);
+    console.error(`There was an error in the demo. Error: ${e.message}. Stack: ${e.stack}`);
 });
+```
