47a5d016d4fea61184b2bb089ffa507d037466d7
Trevin Hofmann <trevinhofmann@gmail.com>
2024-01-05 11:10:08 -0800

    fix: use the correct regex selector for whitespace

--- src/prompts/refactor.ts
+++ src/prompts/refactor.ts
@@ -44,7 +44,7 @@ const titlePattern = /^👑 *([\s\S]*?) *👑\n/;
 const descriptionPattern = /\n🥔 *([\s\S]*?) *🥔\n/;
 const commitMessagePattern = /\n🐴 *([\s\S]*?) *🐴\n/;
 const branchNamePattern = /\n🦀 *([\s\S]*?) *🦀\n/;
-const contentPattern = /\n🤖\w*([\s\S]*?) *🤖$/;
+const contentPattern = /\n🤖\s*([\s\S]*?) *🤖$/;
 
 const getTitle = (str: string) => str.match(titlePattern)?.[1];
 const getDescription = (str: string) => str.match(descriptionPattern)?.[1];
