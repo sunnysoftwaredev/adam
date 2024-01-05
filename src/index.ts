1ce99ac0e6b06a4d8c0ef3089e9700ff7f8e10b2
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-05 10:59:45 -0800

    feat: Enhanced Error Logging in Promise Catch Block

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+
+```typescript
 import './dotenv';
 import demo from './demo';
 
 demo().catch(e => {
-  console.error('There was an error in the demo.', e);
+  console.error(`There was an error in the demo. Error Name: ${e.name}. Error Message: ${e.message}`);
 });
+```
