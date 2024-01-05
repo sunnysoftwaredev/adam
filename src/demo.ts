5001b9a37c8d1a1eca91aeda427ca93884dabbb6
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-05 18:09:57 +0000

    Refactored environment variable verification and improved file randomization and legibility per pull request review

--- src/demo.ts
+++ src/demo.ts
@@ -1,28 +1,33 @@
 import { createGithubPullRequest, getGithubFile, getGithubFiles } from './github';
 import refactor from './prompts/refactor';
 
-const REPOSITORY = process.env.REPOSITORY;
-const BASE_BRANCH_NAME = process.env.BRANCH;
+// Get environment variables and throw error if they're not defined
+const [REPOSITORY, BASE_BRANCH_NAME] = ['REPOSITORY', 'BRANCH'].map((varName) => {
+  const val = process.env[varName];
+  if (val === undefined) {
+    throw new Error(`The ${varName} environment variable is required.`);
+  }
+  return val;
+});
 
-if (REPOSITORY === undefined) {
-  throw new Error('The REPOSITORY environment variable is required.');
-}
-
-if (BASE_BRANCH_NAME === undefined) {
-  throw new Error('The BRANCH environment variable is required.');
-}
-
+// Refactor given file
 const refactorFile = async (fileName: string): Promise<void> => {
   console.log(`Attempting to refactor ${fileName}`);
+
+  // Get the file from github
   const file = await getGithubFile({
     repository: REPOSITORY,
     branchName: BASE_BRANCH_NAME,
     fileName,
   });
+
+  // Attempt refactoring the file
   const pullRequestInfo = await refactor(file);
   if (pullRequestInfo === undefined) {
     return;
   }
+
+  // If refactoring is successful, create a pull request
   await createGithubPullRequest({
     repository: REPOSITORY,
     baseBranchName: BASE_BRANCH_NAME,
@@ -37,20 +42,27 @@ const refactorFile = async (fileName: string): Promise<void> => {
       }
     ],
   });
+
   console.log(`✅ Refactored ${fileName}`);
 };
 
 export default async (): Promise<void> => {
+
+  // Get all files present in the repository
   const files = await getGithubFiles({
     repository: REPOSITORY,
     branchName: BASE_BRANCH_NAME,
   });
+
+  // Filter, randomize and limit the file set
   const filesToRefactor = files
     // Only TypeScript files
     .filter(file => file.endsWith('.ts') || file.endsWith('.tsx'))
-    // Randomize the order
-    .sort(() => Math.random() > 0.5 ? -1 : 1)
+    // Randomize the order (produce a number between -1 and 1 to decide the sort order)
+    .sort(() => Math.random() - 0.5)
     // Limit to 10 files
     .slice(0, 10);
+
+  // Begin refactoring each file
   await Promise.all(filesToRefactor.map(refactorFile));
 };
