86d4d6615513cd4703720316ec9acc2ef1e5dc50
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-05 18:09:36 +0000

    Enhanced error handling by catching unhandled promise rejections and performed minor code refactoring.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import './dotenv';
 import demo from './demo';
 
-demo().catch(e => {
-  console.error('There was an error in the demo.', e);
-});
+async function runDemo() {
+  try {
+    await demo();
+  } catch (e) {
+    console.error('There was an error running the demo.', e);
+    process.exit(1);
+  }
+}
+
+runDemo();
