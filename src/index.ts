0f218d279dc8422cc526e558193ccdeca18137c5
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-07 12:12:42 +0000

    refactor: create 'logError' utility for centralized error logging

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import './dotenv';
 import demo from './demo';
 
+function logError(errorMessage: string, error: Error): void {
+  console.error(errorMessage, error);
+}
+
 demo().catch(e => {
-  console.error('There was an error in the demo.', e);
+  logError('There was an error in the demo.', e);
 });
