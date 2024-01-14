801ff69254539dbf4e25ec2f2320075242a3f03f
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-14 18:09:25 +0000

    refactor: introduce ErrorHandler for improved 'demo' error management

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import './dotenv';
 import demo from './demo';
 
-demo().catch(e => {
-  console.error('There was an error in the demo.', e);
-});
+class ErrorHandler {
+  static handle(error: Error): void {
+    console.error('There was an error in the demo.', error);
+    // This could be extended to include error reporting, etc.
+  }
+}
+
+demo().catch(ErrorHandler.handle);
