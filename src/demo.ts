50bb091b93f361547f6e7ea665150105cd8d4774
SunnySoftwareADAM <sunnysoftwareadam@gmail.com>
2024-01-21 18:10:07 +0000

    refactor(refactorFile): Log message for skipped refactors on no PR info

--- src/demo.ts
+++ src/demo.ts
@@ -19,10 +19,14 @@ const refactorFile = async (fileName: string): Promise<void> => {
     branchName: BASE_BRANCH_NAME,
     fileName,
   });
+
   const pullRequestInfo = await refactor(file);
+
   if (pullRequestInfo === undefined) {
+    console.log(`⏭ Skipped refactoring ${fileName} - No changes necessary.`);
     return;
   }
+  
   await createGithubPullRequest({
     repository: REPOSITORY,
     baseBranchName: BASE_BRANCH_NAME,
@@ -45,6 +49,7 @@ export default async (): Promise<void> => {
     repository: REPOSITORY,
     branchName: BASE_BRANCH_NAME,
   });
+
   const filesToRefactor = files
     // Only TypeScript files
     .filter(file => file.endsWith('.ts') || file.endsWith('.tsx'))
@@ -52,5 +57,6 @@ export default async (): Promise<void> => {
     .sort(() => Math.random() > 0.5 ? -1 : 1)
     // Limit to 10 files
     .slice(0, 10);
+
   await Promise.all(filesToRefactor.map(refactorFile));
 };
